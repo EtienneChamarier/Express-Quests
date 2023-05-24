a6d32426c8ccfc7182bed035c4e591737338e746
etienne chamarier <etienne.chamarier@gmail.com>
2023-05-24 11:31:37 +0200

    quest3

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
-
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 const port = process.env.APP_PORT ?? 5000;
 
@@ -10,20 +10,19 @@ const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
 
-app.get("/", welcome);
-
-
-
 
 const usersHandler = require("./usersHandler");
+const movieHandlers = require("./movieHandlers");
+
 app.get("/api/users", usersHandler.getUser);
 app.get("/api/users/:id", usersHandler.getUserById);
-
-
-
-const movieHandlers = require("./movieHandlers");
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+app.post("/api/users", validateUser, users.postUser);
+
+
 
 app.listen(port, (err) => {
   if (err) {
@@ -32,5 +31,7 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
+
+app.get("/", welcome);
 
 
